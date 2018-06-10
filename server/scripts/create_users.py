df98b01062e81ad84aba3e08b246faaa366ea94f
Carlos A. Ospina <carlosaospinac@gmail.com>
2018-06-10 10:20:42 -0500

    Change gen_users for individual record

--- server/scripts/create_users.py
+++ server/scripts/create_users.py
@@ -4,12 +4,13 @@ from user_queries import UserQueries
 
 
 def main():
-    with open('scripts/test-users.json', 'r') as f:
-        users = json.loads(f.read())
-        for i, user in enumerate(users):
-            print(f'Record {i + 1} of {len(users)}')
-            UserQueries.register(user)
-        print('Done.')
+    user = {
+        "name": "Carlos Andrés Ospina",
+        "email": "luis@company.org",
+        "role": "admin"
+    }
+    UserQueries.register(user)
+    
 
 if __name__ == '__main__':
     main()
